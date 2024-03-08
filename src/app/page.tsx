bca32fb926c2ea7faca8cf2ab3713d09f8affa9a
JC <jcsacra4@gmail.com>
2024-03-08 23:34:38 +0800

    Edited main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,6 @@ export default async function Home() {
   const session = await getServerSession(authOptions);
 
   if (session) {
-    return <main>Welcome this is your Home page.</main>;
+    return <main> Home page.</main>;
   }
 }
