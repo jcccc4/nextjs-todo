5760a66770face7447bcb658cfd748aac1e6cfe7
JC <jcsacra4@gmail.com>
2024-04-30 20:46:36 +0800

    reset mobile and web view

--- src/app/web/layout.tsx
+++ src/app/web/layout.tsx
@@ -6,11 +6,12 @@ import {
   CardDescription,
   CardContent,
 } from "@/components/ui/card";
+import { SheetContent, SheetTrigger, Sheet } from "@/components/ui/sheet";
 import {
   Badge,
-  Bell,
   Home,
   LineChart,
+  Menu,
   Package,
   Package2,
   ShoppingCart,
@@ -21,53 +22,8 @@ import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="grid min-h-screen w-full grid-cols-[80px_minmax(500px,_1fr)_minmax(auto,500px)] lg:grid-cols-[280px_minmax(480px,_1fr)_minmax(auto,440px)] ">
-      <div className="flex h-full flex-col gap-2 bg-red-50 ">
-        <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-          <Link href="/" className="flex items-center gap-2 font-semibold">
-            <Package2 className="h-6 w-6" />
-            <span className="">Acme Inc</span>
-          </Link>
-        </div>
-        <div className="flex-1">
-          <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-            <Link
-              href="#"
-              className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
-            >
-              <Home className="h-4 w-4" />
-              Dashboard
-            </Link>
-            <Link
-              href="#"
-              className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-            >
-              <Package className="h-4 w-4" />
-              Products{" "}
-            </Link>
-          </nav>
-        </div>
-        <div className="mt-auto p-4">
-          <Card x-chunk="dashboard-02-chunk-0">
-            <CardHeader className="p-2 pt-0 md:p-4">
-              <CardTitle>Upgrade to Pro</CardTitle>
-              <CardDescription>
-                Unlock all features and get unlimited access to our support
-                team.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
-              <Button size="sm" className="w-full">
-                Upgrade
-              </Button>
-            </CardContent>
-          </Card>
-        </div>
-      </div>
+    <main className="grid min-h-screen w-full ">
       {children}
-      <div className="bg-blue-400 w-full h-full">
-
-      </div>
     </main>
   );
 };
